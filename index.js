f2580fb8e83c23076367acd5dd7122ee7e317a3d
Julius-93 <julius.irungu@student.moringaschool.com>
2026-07-18 20:05:56 +0300

    Implement function to fetch and display posts

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your code here!
+
+// this function takes in the array of posts and puts them on the page
+function displayPosts(posts) {
+  // grab the ul from the HTML (it has the id post-list)
+  let postList = document.getElementById('post-list');
+
+  // loop through each post one at a time
+  for (let i = 0; i < posts.length; i++) {
+    let post = posts[i];
+
+    // make a new li for this post
+    let li = document.createElement('li');
+
+    // make an h1 and put the title inside it
+    let h1 = document.createElement('h1');
+    h1.textContent = post.title;
+
+    // make a p and put the body inside it
+    let p = document.createElement('p');
+    p.textContent = post.body;
+
+    // put the h1 and p inside the li
+    li.appendChild(h1);
+    li.appendChild(p);
+
+    // put the li inside the ul
+    postList.appendChild(li);
+  }
+}
+
+// async function that houses the fetch
+async function getPosts() {
+  // await the fetch so we wait for the response to come back
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+  // await turning that response into JSON (this is also async)
+  let posts = await response.json();
+
+  // now that we actually have the posts, display them
+  displayPosts(posts);
+}
+
+// kick everything off
+getPosts();
